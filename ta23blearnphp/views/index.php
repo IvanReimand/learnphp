139c04dcfb80d1b8d28d1cbd4eb40809a572c7c8
IvanR <Ivan.Reimand@tptlive.ee>
2025-11-07 01:28:13 +0200

    adhrsthrsjn

--- ta23blearnphp/views/index.php
+++ ta23blearnphp/views/index.php
@@ -1,4 +1,5 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
+
   <main class="container">
     <?php include __DIR__ . '/partials/hero.php'; ?>
     <?php include __DIR__ . '/partials/featured.php'; ?>
